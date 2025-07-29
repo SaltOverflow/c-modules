7e96346b939f216ea27e461a7bed8c5445badf96
Alvin Zhang <alvinzhang201@gmail.com>
2025-07-29 10:19:18 -0400

    added parsing of top-level symbols

--- code/Driver.py
+++ code/Driver.py
@@ -1,7 +1,12 @@
 import argparse
+import os
+from pprint import pprint
 from antlr4 import *
 from parser.CLexer import CLexer
 from parser.CParser import CParser
+from ListenerTopLevel import ListenerTopLevel
+
+std_modules = {"stdio", "stdlib", "unistd"}
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -9,12 +14,23 @@ def parse_args():
     parser.add_argument("-a", "--action", type=int, choices=[0], default=0, help="""
                         what action to take.
                         0 = parse and output text
+                        1 = parse top-level symbols
                         """)
+    parser.add_argument("-r", "--project_root", help="the root folder of the project, used to generate module names")
+    parser.add_argument("-s", "--type_stub_file", help="file that stores type information, for generating type stub headers")
     args = parser.parse_args()
     return args
 
 def main():
     args = parse_args()
+    if args.action == 0:
+        simple_parse(args)
+    elif args.action == 1:
+        parse_top_level(args)
+    else:
+        print(f"Unknown value for {args.action=}")
+
+def simple_parse(args):
     input_stream = FileStream(args.input_file)
     lexer = CLexer(input_stream)
     stream = CommonTokenStream(lexer)
@@ -22,20 +38,126 @@ def main():
     tree = parser.compilationUnit()
     print(tree.getText())
 
-# input_stream = FileStream('testing/just_main.c')
-# lexer = CLexer(input_stream)
-# tokens = lexer.getAllTokens()
-# # tokens[0].text
+def parse_top_level(args):
+    # args.input_file
+    # args.project_root
+    # args.type_stub_file
 
-# input_stream = FileStream('testing/just_main.c')
-# lexer = CLexer(input_stream)
-# stream = CommonTokenStream(lexer)
-# parser = CParser(stream)
-# tree = parser.compilationUnit()
-# # tree.getSourceInterval()  # eg. a,b = tree.getSourceInterval() ; ''.join(i.text for i in tokens[a:b+1])
-# # tree.getChildren()
-# # tree.getChildCount()
-# # tree.getChild()
+    input_file = os.path.relpath(args.input_file, args.project_root)
+    os.chdir(args.project_root)
+    if input_file.endswith('.c'):
+        input_file = input_file[0:len(input_file)-2]
+
+    module_asts = {}  # dict[file_path:str, (tokens, tree)]
+    module_imports = {}  # dict[file_path:str, list[file_path:str]]
+    modules_to_process = [(input_file, input_file)]  # list[(file_path:str, import_name:str)]
+    unknown_modules = set()  # set[(file_path:str, import_name:str)]
+    not_a_module = list()  # list[(file_path:str, import_name:str)]
+    while modules_to_process:
+        file_path, import_name = modules_to_process.pop()
+        if file_path in module_asts:
+            continue
+        try:
+            tokens, tree = parse_file(file_path+'.c')
+        except FileNotFoundError:
+            unknown_modules.add((file_path, import_name))
+            continue
+        if not is_module(tree):
+            not_a_module.append((file_path, import_name))
+        module_asts[file_path] = tokens, tree
+        import_names = list(all_imports(tokens, tree))
+        file_path_dir = os.path.split(file_path)[0]
+        import_file_paths = [os.path.normpath('./'+file_path_dir+'/'+x) for x in import_names]
+        module_imports[file_path] = import_file_paths
+        modules_to_process.extend(zip(import_file_paths, import_names))
+
+    pprint(module_imports)
+
+    if len(not_a_module) > 1 or (len(not_a_module) == 1 and not_a_module[0][0] != input_file):
+        raise RuntimeError(f"Non-modules detected ({input_file} is ok, it's the first file): {not_a_module}")
+    problem_unknown_modules = []
+    for file_path, import_name in unknown_modules:
+        if import_name not in std_modules:
+            problem_unknown_modules.append((file_path, import_name))
+    if problem_unknown_modules:
+        raise RuntimeError(f"Unknown module names detected: {problem_unknown_modules}")
+
+    module_data = {}  # dict[file_path:str, (functions, structs, variables)], see ListenerTopLevel for details
+    module_problems = {}  # dict[file_path: str, (problem_contexts, colliding_contexts)], see ListenerTopLevel for details
+    walker = ParseTreeWalker()
+    for file_path, (_, tree) in module_asts.items():
+        ltoplevel = ListenerTopLevel()
+        walker.walk(ltoplevel, tree)
+        module_data[file_path] = ltoplevel.functions, ltoplevel.structs, ltoplevel.variables
+        if ltoplevel.problem_contexts or ltoplevel.colliding_contexts:
+            module_problems[file_path] = ltoplevel.problem_contexts, ltoplevel.colliding_contexts
+
+    if module_problems:
+        raise RuntimeError(f"Module problems: {module_problems}")
+
+    pprint(module_data)
+    pprint(module_problems)
+
+def all_imports(tokens, tree):
+    translationUnit = tree.getChild(0)
+    if type(translationUnit) != CParser.TranslationUnitContext:
+        return
+    for importDeclaration in translationUnit.getChildren():
+        if type(importDeclaration) != CParser.ImportDeclarationContext:
+            continue
+        numChildren = importDeclaration.getChildCount()
+        start = importDeclaration.getChild(1).getSourceInterval()[0]
+        end = importDeclaration.getChild(numChildren-2).getSourceInterval()[1] + 1
+        import_name = ''.join(x.text for x in tokens[start:end])
+        if import_name.startswith('"'):
+            import_name = import_name[1:len(import_name)-1]
+        yield import_name
+
+def is_module(tree):
+    try:
+        return type(tree.getChild(0).getChild(0)) is CParser.ModuleDeclarationContext
+    except AttributeError:
+        return False
+
+def parse_file(file_path):
+    # Get tokens so we can regenerate the original code
+    input_stream = FileStream(file_path)
+    lexer = CLexer(input_stream)
+    tokens = lexer.getAllTokens()
+    # tokens[0].text
+    #
+    # FileStream is consumed, so need to reopen
+    input_stream = FileStream(file_path)
+    lexer = CLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = CParser(stream)
+    tree = parser.compilationUnit()
+    # tree.getSourceInterval()  # eg. a,b = tree.getSourceInterval() ; ''.join(i.text for i in tokens[a:b+1])
+    # tree.getChildren()
+    # tree.getChildCount()
+    # tree.getChild()
+    #
+    if parser.getNumberOfSyntaxErrors() > 0:
+        raise RuntimeError(f"syntax errors for file {file_path}")
+    return tokens, tree
+
+def details(ast):
+    # Used in debugging
+    print(f"Details for {repr(ast)}")
+    num_auto_expand = 0
+    while ast.getChildCount() == 1:
+        ast = ast.getChild(0)
+        num_auto_expand += 1
+        print(repr(ast))
+    if num_auto_expand:
+        print(f"auto expanded {num_auto_expand} times")
+    n = ast.getChildCount()
+    print(n)
+    for i in range(n):
+        print()
+        print(i)
+        print(repr(ast.getChild(i)))
+        print(ast.getChild(i).getText())
 
 if __name__ == '__main__':
     main()
---
+++ code/ListenerTopLevel.py
@@ -0,0 +1,98 @@
+from antlr4 import *
+from parser.CParser import CParser
+from parser.CListener import CListener
+
+class ListenerTopLevel(CListener):
+    def __init__(self):
+        # helpers
+        self.outer = []  # list[ctx], stores parent contexts (used in parsing and detecting problem contexts)
+        self.variable_names = set()  # list[name:str], holds variable names from self.variables for collision detection (note: only top-level)
+        # errors
+        self.problem_contexts = []  # list[ctx], stores non-supported definitions (non-top level, nameless)
+        self.colliding_contexts = []  # list[(name:str, ctx)], stores definitions with the same name as another
+        # output
+        self.functions = {}  # dict[name:str, ctx:functionDefinitionContext]
+        self.structs = {}  # dict[name:str, ctx:structSpecifierContext]
+        self.variables = []  # list[(list[name:str], ctx:declarationContext)], only contains top-level
+
+    def enterFunctionDefinition(self, ctx):
+        self.outer.append(ctx)
+    
+    def exitFunctionDefinition(self, ctx):
+        should_be_ctx = self.outer.pop()
+        assert(should_be_ctx is ctx)
+        if self.outer:
+            self.problem_contexts.append(ctx)
+            return
+        name = self._extract_name_from_declarator(ctx.declarator())
+        if name in self.functions:
+            self.colliding_contexts.append((name, ctx))
+        else:
+            self.functions[name] = ctx
+
+    def enterStructSpecifier(self, ctx):
+        self.outer.append(ctx)
+    
+    def exitStructSpecifier(self, ctx):
+        should_be_ctx = self.outer.pop()
+        assert(should_be_ctx is ctx)
+        if ctx.getChildCount() == 2:
+            return  # not a struct definition
+        if self.outer:
+            self.problem_contexts.append(ctx)
+            return
+        if not ctx.Identifier():
+            self.problem_contexts.append(ctx)  # anonymous struct, not supported right now
+            return
+        name = ctx.Identifier().getText()
+        if name in self.structs:
+            self.colliding_contexts.append((name, ctx))
+        else:
+            self.structs[name] = ctx
+    
+    def enterDeclaration(self, ctx):
+        if not ctx.initDeclaratorList() and not self._extract_name_from_declarationSpecifiers(ctx.declarationSpecifiers()):
+            return
+        self.outer.append(ctx)
+    
+    def exitDeclaration(self, ctx):
+        if not ctx.initDeclaratorList():
+            name = self._extract_name_from_declarationSpecifiers(ctx.declarationSpecifiers())
+            if not name:
+                return
+        should_be_ctx = self.outer.pop()
+        assert(should_be_ctx is ctx)
+        if self.outer:
+            return  # non-top level variable
+        if not ctx.initDeclaratorList():
+            names = [name]
+        else:
+            names = [self._extract_name_from_declarator(x.declarator()) for x in ctx.initDeclaratorList().initDeclarator()]
+        colliding_names = self.variable_names.intersection(names)
+        if colliding_names:
+            self.colliding_contexts.append((colliding_names.pop(), ctx))  # could grab all the colliding names, but one is enough
+        else:
+            self.variable_names.update(names)
+            self.variables.append((names, ctx))
+
+    def _extract_name_from_declarator(self, ctx:CParser.DeclaratorContext) -> str:
+        directDeclarator = ctx.directDeclarator()
+        while directDeclarator.directDeclarator():
+            directDeclarator = directDeclarator.directDeclarator()
+        if directDeclarator.declarator():
+            return self._extract_name_from_declarator(directDeclarator.declarator())
+        return directDeclarator.Identifier().getText()
+
+    def _extract_name_from_declarationSpecifiers(self, ctx:CParser.DeclarationSpecifiersContext) -> str | None:
+        typeSpecifierParsed = False
+        for declarationSpecifier in ctx.declarationSpecifier():
+            if not declarationSpecifier.typeSpecifier():
+                continue
+            if not typeSpecifierParsed:
+                typeSpecifierParsed = True
+            else:
+                typedefName = declarationSpecifier.typeSpecifier().typedefName()
+                if typedefName:
+                    return typedefName.getText()  # stuff after may be invalid, but I don't check it
+        return None
+        
